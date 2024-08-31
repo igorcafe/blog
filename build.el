9a9794c8fb6aebe4d0a02393c42de0dd334fdb9f
Igor Cafe <igorcafe@protonmail.com>
2024-08-31 02:59:41 -0300

    muitas melhorias no estilo e estrutura do site

--- build.el
+++ build.el
@@ -1,9 +1,11 @@
 (require 'ox-publish)
 
-(setq org-html-validation-link nil            ;; Don't show validation link
-      org-html-head-include-scripts nil       ;; Use our own scripts
-      org-html-head-include-default-style nil ;; Use our own styles
-      org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />")
+(setq org-export-with-broken-links t
+	  org-html-validation-link nil
+	  org-html-head-include-scripts nil
+	  org-html-head-include-default-style nil
+	  org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />
+					 <link rel=\"stylesheet\" href=\"style.css\" />")
 
 (setq org-publish-project-alist
       (list
@@ -18,6 +20,18 @@
 			 :with-toc nil
 			 :time-stamp-file nil
 			 :with-creator nil
+			 ;; :auto-sitemap t	   
+			 ;; :sitemap-function (lambda (title list)
+			 ;; 					 (concat "* Artigos\n"
+			 ;; 							 (mapconcat (lambda (link)
+			 ;; 										  (format "- [[%s][%s]]"
+			 ;; 												  (car link) (cdr link)))
+			 ;; 										list "\n")))
+			 :html-preamble "<nav><a href=\"/\">Inicio</a></nav>"
+			 ;; :sitemap-title "Igor Melo"
+			 ;; :sitemap-filename "index.org"
+			 :with-title t
+			 :sitemap-sort-files 'anti-chronologically
              :publishing-function 'org-html-publish-to-html)))
 
 (org-publish-all t)
