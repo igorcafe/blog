628321067b1ac0d0f062a1e4981028e6c4690a07
Igor Cafe <igorcafe@protonmail.com>
2024-08-30 23:26:34 -0300

    change org publish settings

--- build.el
+++ build.el
@@ -6,6 +6,11 @@
              :recursive t
              :base-directory "./src"
              :publishing-directory "./public"
+			 :with-author nil
+			 :section-numbers nil
+			 :with-creator t
+			 :with-toc nil
+			 :time-stamp-file nil
              :publishing-function 'org-html-publish-to-html)))
 
 (org-publish-all t)
