b2cf6fdb2c743e2f45bfbc16f021fcd8911eda82
Igor Cafe <igorcafe@protonmail.com>
2024-08-30 23:39:39 -0300

    add styles

--- build.el
+++ build.el
@@ -1,6 +1,9 @@
 (require 'ox-publish)
 
-(setq org-html-validation-link nil)
+(setq org-html-validation-link nil            ;; Don't show validation link
+      org-html-head-include-scripts nil       ;; Use our own scripts
+      org-html-head-include-default-style nil ;; Use our own styles
+      org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />")
 
 (setq org-publish-project-alist
       (list
@@ -14,6 +17,7 @@
 			 :with-creator t
 			 :with-toc nil
 			 :time-stamp-file nil
+			 :with-creator nil
              :publishing-function 'org-html-publish-to-html)))
 
 (org-publish-all t)
